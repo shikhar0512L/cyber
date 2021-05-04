8f17b5b241799e40722d337283132aa2d544830e
shikhar0512L <74178918+shikhar0512L@users.noreply.github.com>
2021-05-04 13:02:00 +0530

    Add files via upload

---
+++ sketech.js
@@ -0,0 +1,6 @@
+console.log('Hello Developers');
+alert('Welcome to the website for cyberbullying help');
+
+// Prompt in JS
+const name = prompt("What is your name?", "Guest");
+console.log(name); 
